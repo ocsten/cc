44b7f507601eec623665ac7c151a4ee80566b6ca
ocsten <ysf9xd@gmail.com>
2026-09-03 09:32:19 +0100

    Initialize OCSTEN Framework Modules with metadata

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -1,1 +1,26 @@
 
+# -*- coding: utf-8 -*-
+
+"""
+OCSTEN Framework Modules
+Version: 1.0.0
+"""
+
+__version__ = "1.0.0"
+__author__ = "ocsten"
+
+from .core import OCSTENFramework
+from .network import SessionManager
+from .validators import CardData, validate_card, parse_card
+from .ui import UI
+from .logger import Logger
+
+__all__ = [
+    'OCSTENFramework',
+    'SessionManager',
+    'CardData',
+    'validate_card',
+    'parse_card',
+    'UI',
+    'Logger'
+]
